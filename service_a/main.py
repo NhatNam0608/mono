e00c8742d54419d2beb1b7d701f4db933c41731e
NhatNam0608 <nhatnamit214@gmail.com>
2025-07-14 14:26:31 +0700

    update service-a and service-b

--- service_a/main.py
+++ service_a/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": get_message("service-a-message")}
+    return {"message": get_message("message-from-service-a")}
--- service_b/main.py
+++ service_b/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": get_message("service-b")}
+    return {"message": get_message("message-from-service-b")}
