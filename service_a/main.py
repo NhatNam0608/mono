6730d02546787e00c723f6367fa04a7f978b8d90
NhatNam0608 <nhatnamit214@gmail.com>
2025-07-16 19:03:39 +0700

    fix cicd and testing service a

--- service_a/main.py
+++ service_a/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": get_message("message-from-service-aaaaaaaaa")}
+    return {"message": get_message("message-from-service-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")}
